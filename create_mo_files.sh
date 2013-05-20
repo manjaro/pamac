18acb8ddc64c53969b2cde26c263b1cbed35ce07
guinux <nuxgui@gmail.com>
2013-05-20 11:31:16 +0200

    update translation file

--- create_mo_files.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-for i in `ls po | sed s'|.po||'` ; do
-	mkdir -p "./locale/$i/LC_MESSAGES/"
-	msgfmt -c po/$i.po -o "./locale/$i/LC_MESSAGES/$i.mo"
-done
-itstool -j ./data/polkit/org.manjaro.pamac.policy.in -o ./data/polkit/org.manjaro.pamac.policy `find "./locale/" -name *.mo`
