c6b6681cf1d3c67f2b163faa178cef1be4f0ad05
Philip <philm@manjaro.org>
2015-01-04 09:22:02 +0100

    update language files

--- po/create_pot_file.sh
+++ po/create_pot_file.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 xgettext --from-code=UTF-8 --add-location=file \
-	--package-name=Pamac --package-version=2.0 --msgid-bugs-address=guillaume@manjaro.org \
+	--package-name=Pamac --package-version=2.1 --msgid-bugs-address=guillaume@manjaro.org \
 	--files-from=files_to_translate --keyword=translatable --output=pamac.pot
